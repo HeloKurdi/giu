35b2c3c44394e781606183cfc90b365a8e7cffdc
HeloKurdi <139111061+HeloKurdi@users.noreply.github.com>
2025-06-15 20:42:29 +0300

    Add files via upload

---
+++ Pages/Colleges/Engineering/Departments/Software.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Uniapp.Pages.Colleges.Engineering.Departments
+{
+    public class SoftwareModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+} 
---
+++ Pages/Colleges/Engineering/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Uniapp.Pages.Colleges.Engineering
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+} 
